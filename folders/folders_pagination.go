64a41f4e9c55151ff4f3119f21112e228c3c5595
Jordan Pio <jordan.pio@sca.com.au>
2023-11-13 18:50:24 +1100

    refactor code for better readability

--- folders/folders_pagination.go
+++ folders/folders_pagination.go
@@ -1,11 +1,7 @@
 package folders
 
-// Copy over the `GetFolders` and `FetchAllFoldersByOrgID` to get started
-
 import (
-	"encoding/base64"
 	"fmt"
-	"encoding/json"
 	"github.com/gofrs/uuid"
 )
 
@@ -25,12 +21,11 @@ type PaginatedFetchFolderResponse struct {
 // Pagination logic in GetAllFolders
 func GetPaginatedAllFolders(req *PaginatedFetchFolderRequest) (*PaginatedFetchFolderResponse, error) {
 
-	if req.Cursor == "END_OF_DATA" {
-        return &PaginatedFetchFolderResponse{
-            Folders:    []*Folder{}, // Empty slice
-            NextCursor: "END_OF_DATA",
-        }, nil
+	// Check for end of data
+    if req.Cursor == "END_OF_DATA" {
+        return generateEmptyResponse(), nil
     }
+
     // Use GetAllFolders to fetch all folders
     nonPaginatedResponse, err := GetAllFolders(&FetchFolderRequest{OrgID: req.OrgID})
     if err != nil {
@@ -38,13 +33,42 @@ func GetPaginatedAllFolders(req *PaginatedFetchFolderRequest) (*PaginatedFetchFo
     }
     allFolders := nonPaginatedResponse.Folders
 
-    // Parse the cursor to get the starting point for this page
-    startingAfter, err := parsePaginationToken(req.Cursor)
+	// Paginate folders
+    foldersPage, nextCursor, err := paginateFolders(allFolders, req.Cursor, req.Limit)
     if err != nil {
-        return nil, err // appropriate error handling
+        return nil, err
     }
 
-    // Find the starting index based on the cursor's last ID
+    // Return the paginated response
+    return &PaginatedFetchFolderResponse{
+        Folders:    foldersPage,
+        NextCursor: nextCursor,
+    }, nil
+}
+
+
+func generateEmptyResponse() *PaginatedFetchFolderResponse {
+    return &PaginatedFetchFolderResponse{
+        Folders:    []*Folder{},
+        NextCursor: "END_OF_DATA",
+    }
+}
+
+func paginateFolders(allFolders []*Folder, cursor string, limit int) ([]*Folder, string, error) {
+    startingAfter, err := ParsePaginationToken(cursor)
+    if err != nil {
+        return nil, "", err
+    }
+
+    startIndex, endIndex := calculatePaginationIndexes(allFolders, startingAfter, limit)
+
+    foldersPage := allFolders[startIndex:endIndex]
+    nextCursor := generateNextCursor(foldersPage, endIndex, len(allFolders))
+
+    return foldersPage, nextCursor, nil
+}
+
+func calculatePaginationIndexes(allFolders []*Folder, startingAfter *PaginationTokenStruct, limit int) (int, int) {
     startIndex := 0
     if startingAfter != nil {
         for i, folder := range allFolders {
@@ -55,66 +79,19 @@ func GetPaginatedAllFolders(req *PaginatedFetchFolderRequest) (*PaginatedFetchFo
         }
     }
 
-    // Calculate the endIndex, making sure we don't go beyond the number of available folders
-    endIndex := startIndex + req.Limit
+    endIndex := startIndex + limit
     if endIndex > len(allFolders) {
         endIndex = len(allFolders)
     }
 
-    // Create the paginated slice of folders
-    foldersPage := allFolders[startIndex:endIndex]
-
-    // Generate the next cursor token if there's more data
-	isLastPage := endIndex >= len(allFolders)
-	nextCursor := generatePaginationToken(allFolders[endIndex-1], isLastPage)
-
-    // Return the paginated response
-    return &PaginatedFetchFolderResponse{
-        Folders:    foldersPage,
-        NextCursor: nextCursor,
-    }, nil
-}
-// PaginationTokenStruct defines the structure of the pagination token.
-
-type PaginationTokenStruct struct {
-	LastID uuid.UUID `json:"lastId"`
+    return startIndex, endIndex
 }
 
-// Helper function to generate a pagination token from the last folder in a page
-func generatePaginationToken(lastFolder *Folder, isLastPage bool) string {
-    // If lastFolder is nil, return an empty string (no more data to paginate)
-	if lastFolder == nil || isLastPage {
+func generateNextCursor(foldersPage []*Folder, endIndex, totalFolders int) string {
+    if endIndex >= totalFolders {
         return "END_OF_DATA"
     }
-
-    // Generate token based on the last folder's ID
-    tokenStruct := PaginationTokenStruct{
-        LastID: lastFolder.Id,
-    }
-    tokenBytes, err := json.Marshal(tokenStruct)
-    if err != nil {
-        fmt.Printf("Error generating pagination token: %v\n", err)
-        return ""
-    }
-    return base64.URLEncoding.EncodeToString(tokenBytes)
+    lastFolder := foldersPage[len(foldersPage)-1]
+    return GeneratePaginationToken(lastFolder, false)
 }
 
-// Helper function to parse a pagination token back into the last ID
-func parsePaginationToken(token string) (*PaginationTokenStruct, error) {
-	if token == "" {
-		return nil, nil // No cursor provided, start from the beginning
-	}
-
-	tokenBytes, err := base64.URLEncoding.DecodeString(token)
-	if err != nil {
-		return nil, err
-	}
-
-	var tokenStruct PaginationTokenStruct
-	err = json.Unmarshal(tokenBytes, &tokenStruct)
-	if err != nil {
-		return nil, err
-	}
-
-	return &tokenStruct, nil
-}
